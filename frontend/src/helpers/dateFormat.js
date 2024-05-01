f2025701afd1efcb2d8c5efaf5c5f5d11e5a90e1
DanielRasho <ray22933@uvg.edu.gt>
2024-05-01 12:41:06 -0600

    Fix: Remove unnecesary log

--- frontend/src/helpers/dateFormat.js
+++ frontend/src/helpers/dateFormat.js
@@ -1,6 +1,5 @@
 export default function formatDate(dateString) {
   const date = new Date(dateString);
-  console.log(dateString);
   const options = { month: "short", day: "2-digit", year: "numeric" };
   return date.toLocaleDateString("en-US", options);
 }
